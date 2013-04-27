0d14a9e1b898b4548484f64ae6d36fc1df81b690
Dmitri Shuralyov <shurcooL@gmail.com>
2013-04-27 16:52:18 -0400

    Removed/refactored unneccessary duplication.
    Moved new test for dumping nils to complete_example.go.
    Removed duplicated test from complete_example.go (it's already in
    small_example.go).
    Combined two identical "for all ./tests/*.go files" loops into one "for
    the two commands" loop.

--- goon_test.go
+++ goon_test.go
@@ -1,6 +1,5 @@
 package goon_test
 
-import "github.com/shurcooL/go-goon"
 import "testing"
 
 import (
@@ -11,99 +10,26 @@ import (
 	"strings"
 )
 
-func TestFirst(t *testing.T) {
-	type Inner struct {
-		Field1 string
-		Field2 int
-	}
-	type Lang struct {
-		Name  string
-		Year  int
-		URL   string
-		Inner *Inner
-	}
-
-	x := Lang{
-		Name: "Go",
-		Year: 2009,
-		URL:  "http",
-		Inner: &Inner{
-			Field1: "Secret!",
-			Field2: (int)(0),
-		},
-	}
-
-	want := `(goon_test.Lang)(goon_test.Lang{
-	Name: (string)("Go"),
-	Year: (int)(2009),
-	URL:  (string)("http"),
-	Inner: (*goon_test.Inner)(&goon_test.Inner{
-		Field1: (string)("Secret!"),
-		Field2: (int)(0),
-	}),
-})
-`
-
-	if got := goon.Sdump(x); got != want {
-		t.Errorf("goon.Sdump(%#v) = %v, want %v", x, got, want)
-	}
-}
-
-// TODO: Next step, refactor the common parts out
-func TestSecond(t *testing.T) {
-	x := []int{
-		5,
-		3,
-	}
-
-	want := `([]int)([]int{
-	(int)(5),
-	(int)(3),
-})
-`
-
-	if got := goon.Sdump(x); got != want {
-		t.Errorf("goon.Sdump(%#v) = %v, want %v", x, got, want)
-	}
-}
-
-func TestThird(t *testing.T) {
-	x := (*string)(nil)
-
-	want := `(*string)(nil)
-(interface{})(nil)
-`
-
-	if got := goon.Sdump(x, nil); got != want {
-		t.Errorf("goon.Sdump(%#v) = %v, want %v", x, got, want)
-	}
-}
-
-func TestFourth(t *testing.T) {
+func Test(t *testing.T) {
 	os.Chdir("./tests/")
 	files, err := ioutil.ReadDir("./")
 	CheckError(err)
 
-	for _, file := range files {
-		if !file.IsDir() && strings.HasSuffix(file.Name(), ".go") {
-			filename := file.Name()
-			cmd := "go run \""+filename+"\" > \""+filename+".out\""
-
-			out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
-			if nil != err || 0 != len(out) {
-				t.Errorf("Failed `%s` with err %v and output %q.", cmd, err, string(out))
-			}
-		}
+	cmds := []func(string) string{
+		func(filename string) string { return "go run \"" + filename + "\" > \"" + filename + ".out\"" },
+		func(filename string) string { return "git diff --no-ext-diff -- \"" + filename + ".out\"" },
 	}
 
-	for _, file := range files {
-		if !file.IsDir() && strings.HasSuffix(file.Name(), ".go") {
-			filename := file.Name()
-			cmd := "git diff --no-ext-diff -- \""+filename+".out\""
+	for _, cmd := range cmds {
+		for _, file := range files {
+			if !file.IsDir() && strings.HasSuffix(file.Name(), ".go") {
+				filename := file.Name()
+				cmdString := cmd(filename)
 
-			out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
-			if nil != err || 0 != len(out) {
-				t.Errorf("Failed `%s` with err %v and output %q.", cmd, err, string(out))
+				out, err := exec.Command("bash", "-c", cmdString).CombinedOutput()
+				if nil != err || 0 != len(out) {
+					t.Errorf("Failed `%s` with err %v and output %q.", cmdString, err, string(out))
+				}
 			}
 		}
 	}
--- tests/complete_example.go
+++ tests/complete_example.go
@@ -3,32 +3,16 @@ package main
 import "github.com/shurcooL/go-goon"
 
 func main() {
-	type Inner struct {
-		Field1 string
-		Field2 int
-	}
-	type Lang struct {
-		Name  string
-		Year  int
-		URL   string
-		Inner *Inner
-	}
-
-	x := Lang{
-		Name: "Go",
-		Year: 2009,
-		URL:  "http",
-		Inner: &Inner{
-			Field1: "Secret!",
-		},
-	}
-	goon.Dump(x)
-
 	goon.Dump(map[string]int64{
-		"x": 8,
-		"z": 8,
-		"y": 7,
+		"x": 1,
+		"y": 4,
+		"z": 7,
 	})
 
 	goon.Dump([]int32{1, 5, 8})
+
+	{
+		x := (*string)(nil)
+		goon.Dump(x, nil)
+	}
 }
