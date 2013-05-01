f86b2b603e732b939ea2e755e5d16e67eccea337
Dmitri Shuralyov <shurcooL@gmail.com>
2013-05-01 18:37:02 -0400

    Noop.
    Changed indentation from spaces to tabs.

--- tests/small_example.go
+++ tests/small_example.go
@@ -3,25 +3,25 @@ package main
 import "github.com/shurcooL/go-goon"
 
 func main() {
-    type Inner struct {
-        Field1 string
-        Field2 int
-    }
-    type Lang struct {
-        Name  string
-        Year  int
-        URL   string
-        Inner *Inner
-    }
+	type Inner struct {
+		Field1 string
+		Field2 int
+	}
+	type Lang struct {
+		Name  string
+		Year  int
+		URL   string
+		Inner *Inner
+	}
 
-    x := Lang{
-        Name: "Go",
-        Year: 2009,
-        URL:  "http",
-        Inner: &Inner{
-            Field1: "Secret!",
-        },
-    }
+	x := Lang{
+		Name: "Go",
+		Year: 2009,
+		URL:  "http",
+		Inner: &Inner{
+			Field1: "Secret!",
+		},
+	}
 
-    goon.Dump(x)
+	goon.Dump(x)
 }
